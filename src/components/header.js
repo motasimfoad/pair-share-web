f058fc2a1e86883ddda082e250e080e05d41ad10
milimdali56 <milimdali@gmail.com>
2018-10-03 16:51:06 +0600

    dashboard progress

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import '../styles/header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserEdit,faPen, faShareAlt } from '@fortawesome/free-solid-svg-icons';
-
-
+import { faUserEdit,faPen, faUserFriends, faClipboardList, faHashtag, faTag, faThList, faTags} from '@fortawesome/free-solid-svg-icons';
+import {FormControl} from 'react-bootstrap';
 
 
 class header extends Component {
@@ -15,12 +14,23 @@ class header extends Component {
             <img className="profilepic" src="http://icons-for-free.com/free-icons/png/512/2540590.png" alt="profilepic"></img>
             <img className="personOne" src="http://icons-for-free.com/free-icons/png/512/2540590.png" alt="profilepic"></img>
               <div className="share">
-              <FontAwesomeIcon  className="shareicon" icon={faShareAlt} size={"4x"} />
+              <FontAwesomeIcon  className="shareicon" icon={faUserFriends} size={"4x"} />
               </div>
             </div>
           
           <div className="right">
-            right
+            <div className="logo">
+            <FontAwesomeIcon  className="" icon={faClipboardList} size={"5x"} />
+            <h1>Pair Share</h1>
+            </div>
+              <div className="container">
+              <FormControl className="search" type="text" placeholder="Search" />
+              <h4 className="list"> <FontAwesomeIcon  icon={faThList} /> Add task</h4>
+              <h4 className="list"> <FontAwesomeIcon  icon={faTag} /> Create tag</h4>
+              <h4 className="list"> <FontAwesomeIcon  icon={faHashtag} /> Important</h4>
+              <h4 className="list"> <FontAwesomeIcon  icon={faTags} /> Personal</h4>
+              <h4 className="list"> <FontAwesomeIcon  icon={faTags} /> Work</h4>
+              </div>
           </div>
 
           <div className="name"><h3>Username <FontAwesomeIcon icon={faUserEdit} /></h3>
